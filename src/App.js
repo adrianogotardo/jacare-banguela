4b4a2a0fddb58a0ae8cb9002ce59830b9187a628
Adriano Gotardo <adrianogotardo.12@gmail.com>
2023-12-07 00:15:39 -0300

    Adicionei o background

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import jacareMotorista from './img/jacare-motorista.jpeg';
 import jacarePizza from './img/jacare-pizza.jpeg';
 import jacareMini from './img/jacare-mini.webp';
+import jacareBackground from './img/jacare-background.jpg';
 
 export default function App() {
   function scrollToSection(id) {
@@ -16,53 +17,70 @@ export default function App() {
   };
 
   return (
-    <Container>
-      <Header>
-        <Logo>O Jacaré Banguela</Logo>
-        <Nav>
-          <NavItem onClick={() => scrollToSection('home')}>Início</NavItem>
-          <NavItem onClick={() => scrollToSection('about')}>Sobre</NavItem>
-          <NavItem onClick={() => scrollToSection('author')}>Autor</NavItem>
-          <NavItem onClick={() => scrollToSection('order')}>Comprar</NavItem>
-        </Nav>
-      </Header>
+    <Body>
+      <Background style={{ backgroundImage: `url(${jacareBackground})` }}></Background>
+      <Container>
+        <Header>
+          <Logo>O Jacaré Banguela</Logo>
+          <Nav>
+            <NavItem onClick={() => scrollToSection('home')}>Início</NavItem>
+            <NavItem onClick={() => scrollToSection('about')}>Sobre</NavItem>
+            <NavItem onClick={() => scrollToSection('author')}>Autor</NavItem>
+            <NavItem onClick={() => scrollToSection('order')}>Comprar</NavItem>
+          </Nav>
+        </Header>
 
-      <Section id="home">
-        <Content>
-          <h2>Bem vindo ao site oficial do querido Jacaré Banguela!</h2>
-          <p>Aqui você poderá conhecer um pouquinho mais do nosso amigo desdentado e do Gustavo, autor dessa aventura tâo divertida :)</p>
-        </Content>
-        <img src={jacareMotorista} alt='Jacaré motorista'/>
-      </Section>
+        <Section id="home">
+          <Content>
+            <h2>Bem vindo ao site oficial do querido Jacaré Banguela!</h2>
+            <p>Aqui você poderá conhecer um pouquinho mais do nosso amigo desdentado e do Gustavo, autor dessa aventura tâo divertida :)</p>
+          </Content>
+          <img src={jacareMotorista} alt='Jacaré motorista'/>
+        </Section>
 
-      <Section id="about">
-        <img src={jacarePizza} alt='Jacaré pizza'/>
-        <Content>
-          <h2>Sobre o livro</h2>
-          <p>O Jacaré Banguela adora comer, principalmente peixes, mas constantemente tem se engasgado. Ele não sabe mais o que fazer, então busca ajuda do Dr. Coruja para esse problema resolver.</p>
-          <p>Quer saber como o Banguela consegue comer? Junte-se a nós nesta história rimada e venha se divertir pra valer!</p>
-        </Content>
-      </Section>
+        <Section id="about">
+          <img src={jacarePizza} alt='Jacaré pizza'/>
+          <Content>
+            <h2>Sobre o livro</h2>
+            <p>O Jacaré Banguela adora comer, principalmente peixes, mas constantemente tem se engasgado. Ele não sabe mais o que fazer, então busca ajuda do Dr. Coruja para esse problema resolver.</p>
+            <p>Quer saber como o Banguela consegue comer? Junte-se a nós nesta história rimada e venha se divertir pra valer!</p>
+          </Content>
+        </Section>
 
-      <Section id="author">
-        <Content>
-          <h2>Sobre o autor</h2>
-          <p>O autor dessa aventura é o Gustavo, também chamado de Guguinha Lindão! Ele tem uma família linda, muito amorosa e muito amada por todos que os conhecem.</p>
-          <p>Eles em breve devem se mudar para Nova Friburgo, terra natal dos mini-jacarés: uma raça de jacarés geneticamente alterada que, apesar do pequeno tamanho, têm um cérebro superdesenvolvido capaz de de compreender conceitos de álgebra avançada.</p>
-        </Content>
-        <img src={jacareMini} alt='Jacaré mini'/>
-      </Section>
+        <Section id="author">
+          <Content>
+            <h2>Sobre o autor</h2>
+            <p>O autor dessa aventura é o Gustavo, também chamado de Guguinha Lindão! Ele tem uma família linda, muito amorosa e muito amada por todos que os conhecem.</p>
+            <p>Eles em breve devem se mudar para Nova Friburgo, terra natal dos mini-jacarés: uma raça de jacarés geneticamente alterada que, apesar do pequeno tamanho, têm um cérebro superdesenvolvido capaz de de compreender conceitos de álgebra avançada.</p>
+          </Content>
+          <img src={jacareMini} alt='Jacaré mini'/>
+        </Section>
 
-      <Section id="order">
-        <Content>
-          <h2>Garanta o seu!</h2>
-          <p>O nosso site chegou ao fim, mas sua aventura com o Jacaré Banguela está apenas começando. Compre já algumas unidades para presentar todas as crianças que você conhece e ama.</p>
-          <p>Ah, e aproveite a nossa promoção: na compra de 99 unidades, a centésima sai com 25% de desconto!</p>
-        </Content>
-      </Section>
-    </Container>
+        <Section id="order">
+          <Content>
+            <h2>Garanta o seu!</h2>
+            <p>O nosso site chegou ao fim, mas sua aventura com o Jacaré Banguela está apenas começando. Compre já algumas unidades para presentar todas as crianças que você conhece e ama.</p>
+            <p>Ah, e aproveite a nossa promoção: na compra de 99 unidades, a centésima sai com 25% de desconto!</p>
+          </Content>
+        </Section>
+      </Container>
+    </Body>
   );
 };
+
+const Body = styled.div`
+  position: relative;
+`;
+
+const Background = styled.div`
+  z-index: -1;
+  width: 100vw;
+  height: 400vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: .1;
+`;
 
 const Container = styled.div`
   font-family: 'Arial', sans-serif;
@@ -76,7 +94,6 @@ const Header = styled.header`
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: 1000;
 `;
 
 const Logo = styled.div`
